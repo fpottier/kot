d79064b43397a530d2fa9f5b336f69ebd5b72c8e
François Pottier <francois.pottier@inria.fr>
2025-07-29 11:47:11 +0200

    Simplified scenario.

--- attic/scenarios/invariant_violation.ml
+++ attic/scenarios/invariant_violation.ml
@@ -1,9 +1,5 @@
 (* @14: Failure in a well-formedness check. *)
           open Kot.Deque;;
-          let rec batch_push n x xs =
-            if n = 0 then xs else
-            let xs = push x xs in
-            batch_push (n-1) x xs;;
           let rec batch_inject n xs x =
             if n = 0 then xs else
             let xs = inject xs x in
@@ -14,14 +10,9 @@
 (* @02 *) let x1 = batch_inject 9 x0 11;;
 (* @03 *) let (x2, _) = eject x1;;
 (* @04 *) let x3 = concat x1 x2;;
-(* @05 *) let x4 = batch_inject 26 x1 17;;
-(* @06 *) let (x5, _) = eject x3;;
 (* @07 *) let (x6, _) = eject x3;;
 (* @08 *) let (x7, _) = eject x6;;
-(* @09 *) let (x8, _) = eject x5;;
-(* @10 *) let x9 = empty;;
 (* @11 *) let (x10, _) = eject x7;;
-(* @12 *) let x11 = empty;;
 (* @13 *) let (x12, _) = eject x10;;
 (* @14 *) let (x13, _) = eject x12;;
           check x13;; (* this check fails! *)
