89e5afc675d8b31a70db799db9ac4d21f3e81169
François Pottier <francois.pottier@inria.fr>
2025-07-29 09:59:22 +0200

    play.ml: a shorter example, complete with its prologue.

--- play.ml
+++ play.ml
@@ -1,19 +1,20 @@
 (* dune utop *)
 (* #use "play.ml" *)
-open Kot.Deque;;
+          open Kot.Deque;;
+          let rec batch_push n x xs =
+            if n = 0 then xs else
+            let xs = push x xs in
+            batch_push (n-1) x xs
+          let rec batch_inject n xs x =
+            if n = 0 then xs else
+            let xs = inject xs x in
+            batch_inject (n-1) xs x
 (* @01 *) let x0 = empty;;
-(* @02 *) let x1 = batch_inject 13 x0 18;;
-(* @03 *) let x2 = batch_inject 7 x1 30;;
-(* @04 *) let x3 = concat x2 x2;;
-(* @05 *) let x4 = concat x3 x2;;
-(* @06 *) let x5 = concat x4 x2;;
-(* @07 *) let (x6, _) = eject x4;;
-(* @08 *) let x7 = concat x5 x5;;
-(* @09 *) let x8 = concat x3 x5;;
-(* @10 *) let x9 = concat x7 x6;;
-(* @11 *) let (_, x10) = pop x3;;
-(* @12 *) let _ = pop x7;;
-(* @12: The exception File "src/Deque.ml", line 298, characters 2-8: Assertion failed was raised. *)
-
-(* This is a scenario where [naive_pop] is used outside of the
-   precondition [naive_pop_permitted]. *)
+(* @02 *) let x1 = batch_inject 24 x0 14;;
+(* @03 *) let x2 = concat x1 x1;;
+(* @04 *) let x3 = concat x2 x1;;
+(* @05 *) let x4 = concat x3 x1;;
+(* @07 *) let x6 = concat x4 x1;;
+(* @08 *) let x7 = concat x6 x4;;
+(* @09 *) let _ = pop x7;;
+(* @09: The exception File "src/Deque.ml", line 298, characters 2-8: Assertion failed was raised. *)
